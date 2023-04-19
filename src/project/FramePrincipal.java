3f4f820fcbda6d1ac1a2ce82daca22497ba7003c
Marcus Sandi <tradedomarcus@gmail.com>
2023-04-19 08:25:25 -0300

    feat: add menu optios, register, sales, about and report

--- src/project/FramePrincipal.java
+++ src/project/FramePrincipal.java
@@ -7,6 +7,13 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
 
 public class FramePrincipal extends JFrame {
 
@@ -40,14 +47,64 @@ public class FramePrincipal extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setLocationRelativeTo(null);
 		setTitle("Oficina...");
+		setResizable(false);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu menuCadastro = new JMenu("Cadastro");
+		menuBar.add(menuCadastro);
+		
+		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Usuários");
+		mntmNewMenuItem_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		menuCadastro.add(mntmNewMenuItem_2);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Clientes");
+		menuCadastro.add(mntmNewMenuItem_1);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Profissionais");
+		menuCadastro.add(mntmNewMenuItem);
+		
+		JMenu menuVendas = new JMenu("Movimento/Vendas");
+		menuVendas.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		menuBar.add(menuVendas);
+		
+		JMenu menuRelatorio = new JMenu("Relatórios");
+		menuRelatorio.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		menuBar.add(menuRelatorio);
+		
+		JMenu menuSobre = new JMenu("Sobre");
+		menuSobre.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		menuBar.add(menuSobre);
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Oficina");
+		JLabel lblNewLabel = new JLabel("Oficina do Zé");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Ubuntu", Font.PLAIN, 20));
-		lblNewLabel.setBounds(166, 61, 139, 34);
+		lblNewLabel.setBounds(0, 87, 434, 34);
 		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\vini6\\Documents\\ADS\\Desenvolvimento-desktop\\oficina\\images\\main-image.png"));
+		lblNewLabel_1.setBounds(0, 11, 434, 76);
+		contentPane.add(lblNewLabel_1);
 	}
-
 }
