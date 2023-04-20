1c7a73ec08c5296a527120edb94dd09824b5a6aa
Marcus Sandi <tradedomarcus@gmail.com>
2023-04-20 08:44:11 -0300

    feat: change font to poppins

--- src/project/Message.java
+++ src/project/Message.java
@@ -40,8 +40,8 @@ public class Message extends JDialog {
 		
 		JLabel messageText = new JLabel(message);
 		messageText.setHorizontalAlignment(SwingConstants.CENTER);
-		messageText.setFont(new Font("Ubuntu", Font.BOLD, 22));
-		messageText.setBounds(0, 0, 375, 69);
+		messageText.setFont(new Font("Poppins Light", Font.BOLD, 22));
+		messageText.setBounds(0, 0, 375, 62);
 		contentPanel.add(messageText);
 		{
 			JLabel lblNewLabel = new JLabel("");
