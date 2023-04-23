82a803d20ee4b1560bc967716628a8e4bd5b3309
Marcus Sandi <tradedomarcus@gmail.com>
2023-04-23 11:50:41 -0300

    style: change size of jdialog

--- src/project/RegisterClient.java
+++ src/project/RegisterClient.java
@@ -67,7 +67,7 @@ public class RegisterClient extends JDialog {
 	 * Create the dialog.RegisterClient
 	 */
 	public RegisterClient() {
-		setBounds(100, 100, 732, 726);
+		setBounds(100, 100, 585, 726);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
@@ -80,171 +80,171 @@ public class RegisterClient extends JDialog {
 			JLabel lblNewUser = new JLabel("Cadastrar cliente");
 			lblNewUser.setHorizontalAlignment(SwingConstants.CENTER);
 			lblNewUser.setFont(new Font("Poppins ExtraBold", Font.PLAIN, 20));
-			lblNewUser.setBounds(0, 0, 716, 51);
+			lblNewUser.setBounds(0, 0, 561, 51);
 			contentPanel.add(lblNewUser);
 		}
 		
 		JLabel lblNomeCompleto = new JLabel("Nome completo");
 		lblNomeCompleto.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNomeCompleto.setFont(new Font("Poppins", Font.PLAIN, 14));
-		lblNomeCompleto.setBounds(116, 107, 488, 22);
+		lblNomeCompleto.setBounds(40, 106, 488, 22);
 		contentPanel.add(lblNomeCompleto);
 		
 		inputNome = new JTextField();
 		inputNome.setFont(new Font("Poppins", Font.PLAIN, 14));
-		inputNome.setBounds(116, 128, 488, 25);
+		inputNome.setBounds(40, 127, 488, 25);
 		contentPanel.add(inputNome);
 		inputNome.setColumns(10);
 		
 		JLabel lblRG = new JLabel("RG");
 		lblRG.setHorizontalAlignment(SwingConstants.CENTER);
 		lblRG.setFont(new Font("Poppins", Font.PLAIN, 14));
-		lblRG.setBounds(116, 164, 226, 22);
+		lblRG.setBounds(40, 163, 226, 22);
 		contentPanel.add(lblRG);
 		
 		inputRG = new JTextField();
 		inputRG.setFont(new Font("Poppins", Font.PLAIN, 14));
 		inputRG.setColumns(10);
-		inputRG.setBounds(116, 185, 226, 25);
+		inputRG.setBounds(40, 184, 226, 25);
 		contentPanel.add(inputRG);
 		
 		JLabel lblCPF = new JLabel("CPF");
 		lblCPF.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCPF.setFont(new Font("Poppins", Font.PLAIN, 14));
-		lblCPF.setBounds(378, 164, 226, 22);
+		lblCPF.setBounds(302, 163, 226, 22);
 		contentPanel.add(lblCPF);
 		
 		inputCPF = new JTextField();
 		inputCPF.setFont(new Font("Poppins", Font.PLAIN, 14));
 		inputCPF.setColumns(10);
-		inputCPF.setBounds(378, 185, 226, 25);
+		inputCPF.setBounds(302, 184, 226, 25);
 		contentPanel.add(inputCPF);
 		
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\vini6\\Documents\\ADS\\Desenvolvimento-desktop\\oficina\\images\\user-icon.png"));
-		lblNewLabel.setBounds(0, 37, 716, 70);
+		lblNewLabel.setBounds(0, 37, 561, 70);
 		contentPanel.add(lblNewLabel);
 		
 		inputNascimento = new JTextField();
 		inputNascimento.setFont(new Font("Poppins", Font.PLAIN, 14));
 		inputNascimento.setColumns(10);
-		inputNascimento.setBounds(116, 240, 226, 25);
+		inputNascimento.setBounds(40, 239, 226, 25);
 		contentPanel.add(inputNascimento);
 		
 		JLabel lblNascimento = new JLabel("Data de nascimento");
 		lblNascimento.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNascimento.setFont(new Font("Poppins", Font.PLAIN, 14));
-		lblNascimento.setBounds(116, 219, 226, 22);
+		lblNascimento.setBounds(40, 218, 226, 22);
 		contentPanel.add(lblNascimento);
 		
 		inputEmail = new JTextField();
 		inputEmail.setFont(new Font("Poppins", Font.PLAIN, 14));
 		inputEmail.setColumns(10);
-		inputEmail.setBounds(378, 240, 226, 25);
+		inputEmail.setBounds(302, 239, 226, 25);
 		contentPanel.add(inputEmail);
 		
 		JLabel lblEmail = new JLabel("E-mail");
 		lblEmail.setHorizontalAlignment(SwingConstants.CENTER);
 		lblEmail.setFont(new Font("Poppins", Font.PLAIN, 14));
-		lblEmail.setBounds(378, 219, 226, 22);
+		lblEmail.setBounds(302, 218, 226, 22);
 		contentPanel.add(lblEmail);
 		
 		JLabel lblTel1 = new JLabel("Telefone 1");
 		lblTel1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTel1.setFont(new Font("Poppins", Font.PLAIN, 14));
-		lblTel1.setBounds(116, 276, 226, 22);
+		lblTel1.setBounds(40, 275, 226, 22);
 		contentPanel.add(lblTel1);
 		
 		inputTelefone1 = new JTextField();
 		inputTelefone1.setFont(new Font("Poppins", Font.PLAIN, 14));
 		inputTelefone1.setColumns(10);
-		inputTelefone1.setBounds(116, 297, 226, 25);
+		inputTelefone1.setBounds(40, 296, 226, 25);
 		contentPanel.add(inputTelefone1);
 		
 		inputTelefone2 = new JTextField();
 		inputTelefone2.setFont(new Font("Poppins", Font.PLAIN, 14));
 		inputTelefone2.setColumns(10);
-		inputTelefone2.setBounds(378, 297, 226, 25);
+		inputTelefone2.setBounds(302, 296, 226, 25);
 		contentPanel.add(inputTelefone2);
 		
 		JLabel lblTel2 = new JLabel("Telefone 2");
 		lblTel2.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTel2.setFont(new Font("Poppins", Font.PLAIN, 14));
-		lblTel2.setBounds(378, 276, 226, 22);
+		lblTel2.setBounds(302, 275, 226, 22);
 		contentPanel.add(lblTel2);
 		
 		inputEstado = new JTextField();
 		inputEstado.setFont(new Font("Poppins", Font.PLAIN, 14));
 		inputEstado.setColumns(10);
-		inputEstado.setBounds(116, 354, 226, 25);
+		inputEstado.setBounds(40, 353, 226, 25);
 		contentPanel.add(inputEstado);
 		
 		JLabel lblEstado = new JLabel("Estado");
 		lblEstado.setHorizontalAlignment(SwingConstants.CENTER);
 		lblEstado.setFont(new Font("Poppins", Font.PLAIN, 14));
-		lblEstado.setBounds(116, 333, 226, 22);
+		lblEstado.setBounds(40, 332, 226, 22);
 		contentPanel.add(lblEstado);
 		
 		inputCidade = new JTextField();
 		inputCidade.setFont(new Font("Poppins", Font.PLAIN, 14));
 		inputCidade.setColumns(10);
-		inputCidade.setBounds(378, 354, 226, 25);
+		inputCidade.setBounds(302, 353, 226, 25);
 		contentPanel.add(inputCidade);
 		
 		JLabel lblCidade = new JLabel("Cidade");
 		lblCidade.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCidade.setFont(new Font("Poppins", Font.PLAIN, 14));
-		lblCidade.setBounds(378, 333, 226, 22);
+		lblCidade.setBounds(302, 332, 226, 22);
 		contentPanel.add(lblCidade);
 		
 		JLabel lblRua = new JLabel("Rua");
 		lblRua.setHorizontalAlignment(SwingConstants.CENTER);
 		lblRua.setFont(new Font("Poppins", Font.PLAIN, 14));
-		lblRua.setBounds(116, 390, 488, 22);
+		lblRua.setBounds(40, 389, 488, 22);
 		contentPanel.add(lblRua);
 		
 		inputRua = new JTextField();
 		inputRua.setFont(new Font("Poppins", Font.PLAIN, 14));
 		inputRua.setColumns(10);
-		inputRua.setBounds(116, 411, 488, 25);
+		inputRua.setBounds(40, 410, 488, 25);
 		contentPanel.add(inputRua);
 		
 		inputBairro = new JTextField();
 		inputBairro.setFont(new Font("Poppins", Font.PLAIN, 14));
 		inputBairro.setColumns(10);
-		inputBairro.setBounds(116, 468, 226, 25);
+		inputBairro.setBounds(40, 467, 226, 25);
 		contentPanel.add(inputBairro);
 		
 		JLabel lblBairro = new JLabel("Bairro");
 		lblBairro.setHorizontalAlignment(SwingConstants.CENTER);
 		lblBairro.setFont(new Font("Poppins", Font.PLAIN, 14));
-		lblBairro.setBounds(116, 447, 226, 22);
+		lblBairro.setBounds(40, 446, 226, 22);
 		contentPanel.add(lblBairro);
 		
 		JLabel lvlCEP = new JLabel("CEP");
 		lvlCEP.setHorizontalAlignment(SwingConstants.CENTER);
 		lvlCEP.setFont(new Font("Poppins", Font.PLAIN, 14));
-		lvlCEP.setBounds(378, 447, 226, 22);
+		lvlCEP.setBounds(302, 446, 226, 22);
 		contentPanel.add(lvlCEP);
 		
 		inputCEP = new JTextField();
 		inputCEP.setFont(new Font("Poppins", Font.PLAIN, 14));
 		inputCEP.setColumns(10);
-		inputCEP.setBounds(378, 468, 226, 25);
+		inputCEP.setBounds(302, 467, 226, 25);
 		contentPanel.add(inputCEP);
 		
 		JTextArea inputOBS = new JTextArea();
 		inputOBS.setFont(new Font("Poppins", Font.PLAIN, 13));
 		inputOBS.setLineWrap(true);
 		inputOBS.setWrapStyleWord(true);
-		inputOBS.setBounds(116, 531, 488, 82);
+		inputOBS.setBounds(40, 534, 488, 82);
 		contentPanel.add(inputOBS);
 		
 		JLabel lblOBS = new JLabel("Observações");
 		lblOBS.setHorizontalAlignment(SwingConstants.CENTER);
 		lblOBS.setFont(new Font("Poppins", Font.PLAIN, 14));
-		lblOBS.setBounds(116, 513, 488, 22);
+		lblOBS.setBounds(40, 512, 488, 22);
 		contentPanel.add(lblOBS);
 		{
 			JPanel buttonPane = new JPanel();
