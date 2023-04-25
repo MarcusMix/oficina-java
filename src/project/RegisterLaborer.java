315db8914fca61dd417dabbc4f08271634994d0f
Marcus Sandi <tradedomarcus@gmail.com>
2023-04-25 07:58:30 -0300

    refactor: change name of h1 label to 'title'

--- src/project/RegisterLaborer.java
+++ src/project/RegisterLaborer.java
@@ -88,11 +88,11 @@ public class RegisterLaborer extends JDialog {
 		contentPanel.add(lblNewLabel);
 		
 		{
-			JLabel lblNewUser = new JLabel("Cadastrar profissional");
-			lblNewUser.setHorizontalAlignment(SwingConstants.CENTER);
-			lblNewUser.setFont(new Font("Poppins ExtraBold", Font.PLAIN, 20));
-			lblNewUser.setBounds(0, 0, 561, 51);
-			contentPanel.add(lblNewUser);
+			JLabel title = new JLabel("Cadastrar profissional");
+			title.setHorizontalAlignment(SwingConstants.CENTER);
+			title.setFont(new Font("Poppins ExtraBold", Font.PLAIN, 20));
+			title.setBounds(0, 0, 561, 51);
+			contentPanel.add(title);
 		}
 		
 		JLabel lblNomeCompleto = new JLabel("Nome completo");
