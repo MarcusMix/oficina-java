e6f959ce9d82a8e4377f4fd6746bad655a0ce4b5
Marcus Sandi <tradedomarcus@gmail.com>
2023-04-25 08:00:02 -0300

    refactor: delete current data and fix some code errors

--- db/usuarios.sql
+++ db/usuarios.sql
@@ -1,10 +1,10 @@
 create table usuarios (
 	id int not null auto_increment,
 	nome varchar(60),
-    usuario varchar(10) UNIQUE,
+    usuario varchar(10) unique,
     senha varchar(30),
     primary key (id)
 );
 
-insert into usuarios (nome, usuario, senha )values ('Milena Cristina', 'milena', '123');
+insert into usuarios (nome, usuario, senha ) values ('Milena Cristina', 'milena', '123');
 
