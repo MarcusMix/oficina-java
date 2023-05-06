95fc3a2dfb52d0ea2a0a2d81c2ab9e40a7082058
Marcus Sandi <tradedomarcus@gmail.com>
2023-05-05 22:44:10 -0300

    feat: add client tb with methods

---
+++ src/models/ClientesTB.java
@@ -0,0 +1,139 @@
+package models;
+
+import java.util.List;
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+public class ClientesTB extends AbstractTableModel {
+	
+	private String[] colunas = new String[] { "ID", "Nome", "RG","CPF", "E-mail", "Telefone-1" };
+	
+	private static final int ID = 0;
+	private static final int NOME = 1;
+	private static final int RG = 2;
+	private static final int CPF = 3;
+	private static final int EMAIL = 4;
+	private static final int TELEFONE1 = 5;
+	
+	
+	//clientes model
+	private List<ClienteModel> linhas;
+	
+	public ClientesTB(List<ClienteModel> listaClientes) {
+		linhas = new ArrayList<ClienteModel>(listaClientes);
+	}
+	
+	@Override
+	public int getRowCount() {
+		return linhas.size();
+		
+	}
+	
+	@Override
+	public int getColumnCount() {
+		return colunas.length;
+	}
+	
+	@Override
+	public String getColumnName(int columnIndex) {
+	    return colunas[columnIndex];
+	};
+	
+	@Override
+	public boolean isCellEditable(int rowIndex, int columnIndex) {
+	    return false;
+	}
+	
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+	    // Pega o local referente a linha especificada.
+		ClienteModel clientes = linhas.get(rowIndex);
+	 
+	    switch (columnIndex) {
+	    case ID:
+	        return clientes.getId();
+	    case NOME:
+	        return clientes.getNome();
+	    case RG:
+	        return clientes.getRg();
+	    case CPF:
+	        return clientes.getCpf();
+	    case EMAIL:
+	        return clientes.getEmail();
+	    case TELEFONE1:
+	        return clientes.getFone1();
+
+	    
+	    default:
+	    
+	        throw new IndexOutOfBoundsException("columnIndex out of bounds");
+	    }}
+	    
+	    @Override
+		public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+		    // Seta o local referente a linha especificada.
+			ClienteModel clientes = linhas.get(rowIndex);
+			
+		 
+		    switch (columnIndex) {
+		    case ID:
+		    	clientes.setId((String) aValue);
+		        break;
+		    case NOME:
+		    	clientes.setNome((String) aValue);
+		    	break;
+		    case RG:
+		    	clientes.setRg((String) aValue);
+		    	break;
+		    case CPF:
+		    	clientes.setCpf((String) aValue);	
+		    	break;
+		    case EMAIL:
+		    	clientes.setEmail((String) aValue);
+		    	break;
+		    case TELEFONE1:
+		    	clientes.setFone1((String) aValue);
+		        break;
+		    default:
+		        throw new IndexOutOfBoundsException("columnIndex out of bounds");
+		    }
+		     
+		    fireTableCellUpdated(rowIndex, columnIndex); // Notifica a atualização da célula
+		
+	}
+	    
+	 // Remove o local  da linha especificada.
+	 		public void removeLocal(int indiceLinha) {
+	 		    // Remove o registro.
+	 		    linhas.remove(indiceLinha);
+	 		 
+	 		    // Notifica a mudança.
+	 		    fireTableRowsDeleted(indiceLinha, indiceLinha);
+	 		}
+	 		 
+	 		// Adiciona uma lista de local  no final da lista.
+	 		public void addListaLocais(List<ClienteModel> clientes) {
+	 		    // Pega o tamanho antigo da tabela, que servirá
+	 		    // como índice para o primeiro dos novos registros
+	 		    int indice = getRowCount();
+	 		 
+	 		    // Adiciona os registros.
+	 		    linhas.addAll(clientes);
+	 		 
+	 		    // Notifica a mudança.
+	 		    fireTableRowsInserted(indice, indice + clientes.size());
+	 		}
+	 		 
+	 		// Remove todos os registros.
+	 		public void limpar() {
+	 		    // Remove todos os elementos da lista de sócios.
+	 		    linhas.clear();
+	 		 
+	 		    // Notifica a mudança.
+	 		    fireTableDataChanged();
+	 		}
+	 		
+
+
+}
