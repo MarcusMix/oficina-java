fce9ae442db0f3ad86706067125e329f81c92e5d
Marcus Sandi <tradedomarcus@gmail.com>
2023-05-06 11:37:57 -0300

    feat: create function of controller user and start clients

---
+++ src/controllers/UserController.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import models.UserModel;
+
+public class UserController {
+	
+	public static void handleDataLoginUser(String QUERY, JTextField inputSenha, JTextField inputNome, JTextField inputUser) {
+		
+		UserModel.registerUser(QUERY, inputSenha, inputUser, inputNome);	
+	}
+
+	public static void handleDataSearchUser(String QUERY, JPasswordField inputSenha, JTextField inputNome, JTextField inputUser) {
+		
+		UserModel.searchUser(QUERY, inputSenha, inputUser, inputNome);
+	}
+
+	public static void handleDataDeleteUser(String QUERY, JTextField inputUser, JTextField inputNome, JPasswordField inputSenha) {
+		
+		UserModel.deleteUser(QUERY, inputSenha, inputUser, inputNome);
+	}
+
+}
