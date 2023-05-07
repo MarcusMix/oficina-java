d9e2cc8d3a041e00e43202c55a22e2e33d96f396
Marcus Sandi <tradedomarcus@gmail.com>
2023-05-07 19:29:25 -0300

    feat: init main of project

---
+++ src/controllers/Main.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+public class Main {
+
+	
+	public void main(String[] args) {
+//		SwingUtilities.invokeLater(TabelaClientes2::new);
+	}
+
+}
