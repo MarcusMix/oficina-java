c23d8c7b08963f856a95b90153c34bca28f07a07
Marcus Sandi <tradedomarcus@gmail.com>
2023-05-07 19:32:20 -0300

    feat: methods of search, delete and update clients

--- src/controllers/ClientsController.java
+++ src/controllers/ClientsController.java
@@ -1,8 +1,11 @@
 package controllers;
 
+import javax.swing.JComboBox;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+import models.ClientEachModel;
 import models.ClientModel;
 
 public class ClientsController {
@@ -13,5 +16,54 @@ public class ClientsController {
 
 	public static void handleDataClients(JTable table) {
 		ClientModel.loadingInfoClients(table);
+	}
+
+	public static void handleRegisterClient(
+			String QUERY, 
+			JTextField inputNome, 
+			JTextField inputRG, 
+			JTextField inputCPF, 
+			JTextField inputRua, 
+			JTextField inputBairro,
+			JTextField inputCidade, 
+			JComboBox comboBoxEstado,
+			JTextField inputCEP, 
+			JTextField inputEmail, 
+			JTextField inputTelefone1, 
+			JTextField inputTelefone2, 
+			JTextArea inputOBS, 
+			JTextField inputNascimento)
+	
+	{
+		ClientModel.registerClient(
+				QUERY, 
+				inputNome, 
+				inputRG, 
+				inputCPF, 
+				inputRua, 
+				inputBairro, 
+				inputCidade, 
+				comboBoxEstado, 
+				inputCEP, 
+				inputEmail, 
+				inputTelefone1, 
+				inputTelefone2, 
+				inputOBS, 
+				inputNascimento
+				);
+		
+	}
+
+	public static void handleSearchEachClient(String QUERY, JTextField inputNome, JTextField inputCPF, JTable table) {
+		ClientEachModel.searchEachClient(QUERY, inputNome, inputCPF, table);
+		
+	}
+
+	public static void grabClientsInDb(JTable table) {
+		ClientEachModel.grabClients(table);
+	}
+
+	public static void handleDeleteClient(String QUERY, JTextField inputNome, JTextField inputCPF) {
+		ClientEachModel.deleteClients(QUERY, inputNome, inputCPF);
 	}
 }
