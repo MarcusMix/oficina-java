99d87dbd36fa0af5a488dbd14f5f40d6f0cd673f
Marcus Sandi <tradedomarcus@gmail.com>
2023-05-10 07:49:51 -0300

    feat: implement methods

--- src/controllers/LaborerController.java
+++ src/controllers/LaborerController.java
@@ -1,5 +1,49 @@
 package controllers;
 
+import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField;
+import javax.swing.JTextField;
+
+import models.laborer.LaborerModel;
+
 public class LaborerController {
-	//functions
+
+	public static void handleRegisterLaborer(
+			String QUERY,
+			JTextField inputNome, 
+			JTextField inputRG, 
+			JTextField inputCPF,
+			JTextField inputRua, 
+			JTextField inputBairro, 
+			JTextField inputCidade, 
+			JComboBox comboBoxEstado,
+			JFormattedTextField inputCEP, 
+			JTextField inputEmail, 
+			JFormattedTextField inputTelefone1,
+			JFormattedTextField inputTelefone2, 
+			JComboBox comboBoxCargo, 
+			JTextField inputNascimento,
+			JTextField inputDataAdmissao, 
+			JComboBox comboBoxFuncao
+			) {
+
+		LaborerModel.registerLaborer(
+				QUERY,
+				inputNome, 
+				inputRG, 
+				inputCPF,
+				inputRua, 
+				inputBairro, 
+				inputCidade, 
+				comboBoxEstado,
+				inputCEP, 
+				inputEmail, 
+				inputTelefone1,
+				inputTelefone2, 
+				comboBoxCargo, 
+				inputNascimento,
+				inputDataAdmissao, 
+				comboBoxFuncao);
+
+	}
 }
